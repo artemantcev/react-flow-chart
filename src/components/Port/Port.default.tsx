ae97acbf91269298dfc61004f6035247de5d6251
Artem Antcev <artem.antcev@wemakesoftware.eu>
2020-11-13 18:51:03 +0200

    fixed tslint errors

--- src/components/Port/Port.default.tsx
+++ src/components/Port/Port.default.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import {IConfig, INode, IPort} from '../../'
+import { IConfig, INode, IPort } from '../../'
 
 export interface IPortDefaultProps {
   className?: string
